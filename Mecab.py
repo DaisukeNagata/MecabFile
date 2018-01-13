c50f60dc2f7fcd382db1288fc97950fd85aab4a3
daisukenagata <dbank0208@gmail.com>
2018-01-13 23:55:47 +0900

    add

---
+++ Mecab.py
@@ -0,0 +1,38 @@
+import re
+import bs4
+import sys
+import MeCab
+import urllib.request
+import lxml.html
+
+if len(sys.argv) == 2:
+    url = sys.argv[1]
+else:
+    exit()
+
+soup = bs4.BeautifulSoup(urllib.request.urlopen(url).read(),"html.parser")
+
+title        = soup.title.string
+description  = soup.find(attrs={"name":re.compile(r'Description',re.I)}).attrs['content']
+h1           = soup.h1.string
+contents     = title + description + h1
+output_words = []
+
+
+m = MeCab.Tagger('-Ochasen')
+result = m.parse(contents)
+
+
+for i in result.split('\n'):
+    base_path = "./"
+    insert = base_path + "insert.tex"
+    word = i.split('\t')[0]
+
+    if word == 'EOS':
+        break
+    else:
+        output_words.append(word)
+
+    f = open(insert,"w")
+    f.write(str(output_words))
+    f.close()
