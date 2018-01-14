b8a0e1b157afb3bad5a917646d2a9364601ba74f
daisukenagata <dbank0208@gmail.com>
2018-01-14 18:30:46 +0900

    add word2vec

--- Mecab.py
+++ Mecab.py
@@ -3,6 +3,7 @@ import bs4
 import sys
 import MeCab
 import urllib.request
+from gensim.models import word2vec
 
 if len(sys.argv) == 2:
     url = sys.argv[1]
@@ -23,7 +24,7 @@ result = m.parse(contents)
 
 
 for i in result.split('\n'):
-    word = i.split('\t')
+    word = i.split('\t')[0]
     base_path = "./"
     insert = base_path + "insert.html"
 
@@ -35,3 +36,13 @@ for i in result.split('\n'):
     f = open(insert,"w")
     f.write('<meta http-equiv="content-type" charset="utf-8">' + str(output_words))
     f.close()
+
+model = word2vec.Word2Vec(output_words,
+                          size=100,
+                          min_count=5,
+                          window=5,
+                          iter=5)
+
+print(model)
+
+model.save("word2vec.gensim.model")
