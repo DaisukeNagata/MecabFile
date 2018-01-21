573392311b0694d6b31b4916552688cd8c838c53
daisukenagata <dbank0208@gmail.com>
2018-01-22 04:26:34 +0900

    add

---
+++ example/mecabeEc2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# HTTPヘッダ
+print ("Content-type: text/html\n")
+
+import cgi
+
+# クエリを取得
+form = cgi.FieldStorage()
+course_name = form.getvalue('course', '')
+
+
+import MeCab
+mecab = MeCab.Tagger("-Ochasen")
+print(mecab.parse(course_nam))
+
+
+# HTML
+print ("<html>")
+# HTMLヘッダ
+print ("<head>")
+print ("<title>フォームからのデータ処理のテスト</title>")
+print ("</head>")
+# HTMLボディ
+print ("<body>")
+print (course_name)
+print ("</body>")
+print ("</html>")
