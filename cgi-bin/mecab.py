9a8e83b5a378aa39f25936b9c486d63143a6e4e0
daisukenagata <dbank0208@gmail.com>
2018-08-21 03:37:01 +0900

    add json形式

--- cgi-bin/mecab.py
+++ cgi-bin/mecab.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 import cgi
+from flask import Flask, jsonify, abort, make_response
+import json
+import MeCab
 
-# クエリを取得
 form = cgi.FieldStorage()
 course_name = form.getvalue('say', '')
 
-import MeCab
 mecab = MeCab.Tagger("-Ochasen")
 result = mecab.parse(course_name)
-
 clazz = result.split()
-print(clazz)
 if clazz[0] != 'EOS':
+    result = {
+        "result":True,
+        "data":{
+            "文法":clazz[3]
+        }
+    }
     print('Content-type: text/html; charset=UTF-8\r\n')
-    print(clazz[3])
+    #json.dumps(result, ensure_ascii=False,indent=2)
+    print(result)
