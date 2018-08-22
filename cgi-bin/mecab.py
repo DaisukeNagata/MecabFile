d149fbc8ab8a1b69ec2d97f16502a5990e9f1c1e
daisukenagata <dbank0208@gmail.com>
2018-08-23 01:52:19 +0900

    add json's response

--- cgi-bin/mecab.py
+++ cgi-bin/mecab.py
@@ -5,19 +5,27 @@ from flask import Flask, jsonify, abort, make_response
 import json
 import MeCab
 
-form = cgi.FieldStorage()
-course_name = form.getvalue('say', '')
+#form = cgi.FieldStorage()
+#course_name = form.getvalue('say', '')
 
-mecab = MeCab.Tagger("-Ochasen")
-result = mecab.parse(course_name)
-clazz = result.split()
-if clazz[0] != 'EOS':
-    result = {
-        "result":True,
-        "data":{
-            "文法":clazz[3]
+api = Flask(__name__)
+
+# GETの実装
+@api.route('/<string:users>', methods=['GET'])
+def get_user(users):
+    print(users)
+    mecab = MeCab.Tagger("-Ochasen")
+    result = mecab.parse(users)
+    clazz = result.split()
+    if clazz[0] != 'EOS':
+        result = {
+            "result":True,
+            "data":{
+                "文法":clazz[3]
+            }
         }
-    }
-    print('Content-type: text/html; charset=UTF-8\r\n')
-    #json.dumps(result, ensure_ascii=False,indent=2)
-    print(result)
+        return make_response(json.dumps(result, ensure_ascii=False,indent=2))
+        print('Content-type: text/html; charset=UTF-8\r\n')
+        print(result)
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=8000)
